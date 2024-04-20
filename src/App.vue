44cf7432318558294017997fa513ed8e09753b7f
anubha <anubha.rajput@innotechteam.in>
2024-04-20 18:52:44 +0530

    created

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,64 @@
-<script setup>
-
+<script>
+import Result from './Components/Result.vue'
+export default {
+    data() {
+        return {
+            showBtn: true,
+        }
+    },
+    methods: {
+        getRandomTime() {
+            console.log(Math.floor(Math.random() * 3000) + 2000);
+            return Math.floor(Math.random() * 3000) + 2000;
+        }
+    },
+    components: {
+        Result
+    }
+}
 </script>
 
 <template>
-<h1>Hello</h1>
+    <div class="main-container">
+        <div :class="{ 'card': showBtn === true, 'card2': showBtn !== true }" id="card1" @click="showBtn = !showBtn">
+            <h1 v-if="showBtn">Go</h1>
+            <h1 v-else>Stop</h1>
+        </div>
+        <Result />
+    </div>
 </template>
 
 <style scoped>
+.main-container {
+    margin-top: 50px;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    font-family: sans-serif;
+}
 
+.card {
+    width: 60%;
+    margin: auto;
+    height: 200px;
+    border-radius: 10px;
+    background-color: rgba(86, 223, 86, 0.679);
+}
+
+.card2 {
+    width: 60%;
+    margin: auto;
+    height: 200px;
+    border-radius: 10px;
+    background-color: rgba(251, 41, 41, 0.768);
+}
+
+#card1 {
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 2rem;
+    cursor: pointer;
+}
 </style>
---
+++ src/Components/Result.vue
@@ -0,0 +1,28 @@
+<template>
+ <div class="container">
+    <h4>Click Go to test your reaction time!</h4>
+ </div>
+</template>
+<script>
+export default{
+    data()
+    {
+        return{
+
+        }
+    }
+}
+</script>
+<style>
+.container{
+    width:60%;
+    margin:auto;
+    height:200px;
+    background-color: azure;
+    border-radius:10px;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    }
+</style>
