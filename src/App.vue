77d3e13dea450994961a3255f8619eeedae1a433
anubha <anubha.rajput@innotechteam.in>
2024-04-24 12:14:32 +0530

    add functionality and change layout

--- src/App.vue
+++ src/App.vue
@@ -1,103 +1,65 @@
 <script>
 import Result from './Components/Result.vue';
-import ShowCard from './Components/ShowCard.vue';
-
 export default {
     data() {
         return {
-            maxValue: 0,
             showBtn: true,
             backdrop: true,
-            reactionTime: 0,
-            timeDiff: 0,
-            showModelComponent: false,
-            handleResult: null,
-            storeTime: 0,
-            changeBackgroundColor: true,
-            buttonType:"start",
-            instructionMsz:"Click Go to test your reaction time!",
-            showResult:false,
-            highScore:0
+            startTime: null,
+            reactionTime: null,
+            buttonType: "Go",
+            isGameStopped: false
         }
     },
     methods: {
         changeBackground() {
-            this.storeTime = setInterval(() => {
+            this.storeTime = setTimeout(() => {
                 this.backdrop = !this.backdrop;
-                this.reactionTime = Date.now();
+                this.startTime = Date.now();
+                this.isGameStopped = true;
             }, Math.floor(Math.random() * (10000 - 3000 + 1)) + 3000);
         },
         getReactionTime() {
-            if(this.buttonType==='start')
-            this.buttonType='stop'
-        else
-        this.buttonType='start'
-        this.timeDiff = Date.now() - this.reactionTime;
-        if(this.handleResult===0 && this.buttonType==="start" && this.timeDiff===0){
-            this.instructionMsz='Click Go to test your reaction time!';
-            console.log(this.timeDiff);
-           }
-            else if (this.reactionTime === 0 && this.buttonType==="stop")
-                this.instructionMsz = "Pay attention. Click stop when the color changes.";
-            else if (this.reactionTime === 0 && this.buttonType==="start")
-                this.instructionMsz = "Too quick... Try again!";
-            else if (this.reactionTime !== 0) {
-                this.instructionMsz="your score is"
-                this.changeBackgroundColor=false;
-                console.log("clear");
-                this.showResult=true;
-        }
-        },
-        showModel() {
-            this.showModelComponent = !this.showModelComponent;
-            if(this.showModelComponent===false){
-            this.changeBackgroundColor=true;
-            console.log(this.changeBackgroundColor);
+            if (this.showBtn)
+                this.buttonType = "Go";
+            else
+                this.buttonType = "Stop"
+            if (this.buttonType === "Stop") {
+                this.isGameStopped = false;
+                this.changeBackground();
+            }
+            else {
+                if (this.startTime !== null) {
+                    this.reactionTime = Date.now() - this.startTime;
+                    this.startTime = null;
+                }
+                if (this.isGameStopped === false) {
+                    clearTimeout(this.storeTime);
+                    this.startTime = null;
+                    this.reactionTime = null;
+                }
             }
         },
-        updateMaxValue(value) {
-            this.maxValue = value;
-       
-   
-    }
-    },
-    watch: {
-    changeBackgroundColor(newValue) {
-        if (newValue === true) {
-            console.log("started");
-            this.handleResult=0;
-            this.changeBackground(); 
-            this.showResult=false;
-            this.maxValue=0;
-            this.timeDiff=0;
-            this.reactionTime=0;
-            this.instructionMsz='Click Go to test your reaction time!'
-
-        } else {
-            clearInterval(this.storeTime); 
+        updateIsGameStopped(value) {
+            this.isGameStopped = value;
         }
+
     },
-   },
     components: {
         Result,
-        ShowCard
-    },
-    mounted() {
-        this.changeBackground();
-    },
-    
-
+    }
 }
 </script>
 
 <template>
-    <div class="main-container" :class="{'background-color-green': backdrop, 'background-color-blue': !backdrop}">
-        <div :class="{ 'card': showBtn === true, 'card2': showBtn !== true} " id="card1"  @click="showBtn=!showBtn, getReactionTime()">
-           {{ buttonType }}
+    <div class="main-container" :class="{ 'background-color-blue': backdrop, 'background-color-green': !backdrop }">
+        <div :class="[buttonType === 'Stop' ? 'card-red' : 'card-green']" id="card1"
+            @click="showBtn = !showBtn, getReactionTime()">
+            {{ buttonType }}
         </div>
-        <Result :instructionMsz="instructionMsz" :timeDiff="timeDiff" :maxValue="maxValue" @update:maxValue="updateMaxValue" :showModel="showModel" :showResult="showResult" :highScore="highScore"/>
+        <Result :buttonType="buttonType" :reactionTime="reactionTime" :isGameStopped="isGameStopped"
+            @update:isGameStopped="updateIsGameStopped"></Result>
     </div>
-    <ShowCard v-if="showModelComponent" :showModel="showModel" :maxValue="maxValue"/>
 </template>
 
 <style scoped>
@@ -110,38 +72,34 @@ export default {
     height: 100vh;
 }
 
-.card {
-    width: 60%;
-    margin:0 auto;
-    height: 200px;
-    border-radius: 10px;
+.card-green {
     background-color: rgba(86, 223, 86, 0.679);
 }
 
-.card2 {
-    width: 60%;
-    margin:  0px auto;
-    height: 200px;
-    border-radius: 10px;
+.card-red {
     background-color: rgba(251, 41, 41, 0.768);
 }
 
 #card1 {
+    width: 50%;
+    margin: 0px auto;
+    margin-top: 50px;
+    height: 250px;
+    border-radius: 10px;
     color: white;
     display: flex;
     justify-content: center;
     align-items: center;
     font-size: 2rem;
     cursor: pointer;
+    font-size: 3rem;
 }
-.background-color-green{
+
+.background-color-green {
     background-color: green;
 }
-.background-color-blue{
+
+.background-color-blue {
     background-color: rgb(24, 187, 227);
 }
-</style> 
-
-
-
-
+</style>
--- src/Components/Result.vue
+++ src/Components/Result.vue
@@ -1,50 +1,89 @@
 <template>
- <div class="container">
-   <h4 v-if="showResult">{{instructionMsz }} 
-  {{ maxValue }}</h4>
-  <h4 v-else>{{ instructionMsz }}</h4>
-  <h4 v-if="highScore!==0"> High Score is={{ highScore }}</h4>
- </div>
+  <div class="container">
+    <div v-if="scoreMessage">
+      Your reaction time was <span v-if="hours">{{ hours }} hour </span>
+      <span v-if="minutes">{{ minutes }} min </span> <span v-if="seconds"> {{ seconds }} seconds</span> 0.{{ milisec }}
+    </div>
+    <div v-if="instructionMessage">{{ instructionMessage }}</div>
+    <div v-if="highScoreMessage">{{ highScoreMessage }}</div>
+  </div>
 </template>
+
 <script>
-export default{
-    props:['instructionMsz','timeDiff','maxValue', 'showModel','showResult','highScore'],
-    data()
-    {
-        return{
-            arr:[],
-            currentValue:0
-        }
+
+export default {
+  data() {
+    return {
+      instructionMessage: "Click Go to test your reaction time!",
+      scoreMessage: null,
+      highScoreMessage: null,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+      milisec: 0,
+      highScore: null,
+      CurrentScore: null,
+      arr: []
+    }
+  },
+  props: ["buttonType", "reactionTime", "isGameStopped"],
+  methods: {
+    getUnitsFromTime(ms) {
+      let totalSeconds = Math.floor(ms / 1000);
+      const hr = Math.floor(totalSeconds / 3600);
+      totalSeconds %= 3600;
+      const min = Math.floor(totalSeconds / 60);
+      const sec = totalSeconds % 60;
+      const milisecond = ms % 1000;
+
+      return { hr, min, sec, milisecond };
     },
-    watch: {
-    timeDiff(newValue) {
-        
-      if (this.showResult) {
-        if(this.arr.length===0){
-        this.currentValue=this.timeDiff;
-        this.$emit('update:maxValue', this.timeDiff);
-        this.showModel();
-        }
-        else if(this.currentValue<this.timeDiff)
-        {
-          this.$emit('update:maxValue', this.timeDiff-this.maxValue);
-          this.currentValue=this.timeDiff
-        }
+    getHightScore() {
+      this.CurrentScore = this.reactionTime
+      if (this.arr.length === 0) {
+        this.highScore = this.reactionTime;
+        this.arr.push(this.reactionTime);
+      }
+      else if (this.highScore > this.CurrentScore) {
+        this.arr.push(this.CurrentScore);
+        this.highScore = this.CurrentScore;
+      }
+      this.$emit("update:isGameStopped", false);
     }
-  }
-}
-}
+  },
+  updated() {
+    if (this.buttonType === "Stop") {
+      this.instructionMessage = "Pay attention. Click stop when color changes";
+      this.scoreMessage = null;
+      this.highScoreMessage = null;
+    } else if (this.reactionTime === null && this.buttonType === "Go") {
+      this.instructionMessage = `Too quick... Try again!`;
+    } else if (this.reactionTime && this.buttonType === "Go") {
+      this.getHightScore();
+      const { hr, min, sec, milisecond } = this.getUnitsFromTime(this.CurrentScore);
+      this.scoreMessage = true;
+      this.hours = hr;
+      this.minutes = min;
+      this.seconds = sec;
+      this.milisec = milisecond;
+      this.instructionMessage = "Click Go to test your reaction time!";
+      this.highScoreMessage = `Your High Score is 0.${this.highScore}`;
+    }
+    if (this.reactionTime) {
+      this.getUnitsFromTime(this.reactionTime);
+    }
+  },
+};
 </script>
-<style>
-.container{
-    width:60%;
-    margin:0px auto;
-    height:200px;
-    background-color: azure;
-    border-radius:10px;
-    padding: 10px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    }
+<style scoped>
+.container {
+  width: 50%;
+  margin: 0px auto;
+  background-color: white;
+  padding: 20px 10px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
 </style>
